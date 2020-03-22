12eb9c0dfa9677ec64cbe58eec665323c9f82851
Joseph-Davies <daviesjd1@gmail.com>
2020-03-23 09:47:00 +1100

    removed cell class

--- project_2.py
+++ project_2.py
@@ -1,29 +1,3 @@
 #/usr/bin/python
 #Joseph Davies
 
-class cell:
-	new_id = 0
-	
-	def __init__(self, from_list = [], to_list = []):
-		self._id = new_id + 1
-		new_id += 1
-		
-		self._from = from_list
-		self._to = to_list
-	
-	def get_id(self):
-		return self._id
-	
-	@property
-	def cells_to(self):
-		return self._to
-	@cells_to.setter
-	def cells_to(self, val):
-		self._to = val
-	
-	@property
-	def cells_from(self):
-		return self._from
-	@cells_from.setter
-	def cells_from(self, val):
-		self._from = val
